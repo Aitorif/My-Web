e2119e428cd9a5a50bbcc1036b65e5feb7e6e608
Aitorif <78684596+Aitorif@users.noreply.github.com>
2021-04-06 10:45:57 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+
+
+let miImage = document.querySelector('img');
+miImage.onclick = function () {
+    let miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/guitarra.jpg') {
+      miImage.setAttribute('src','images/1.jpg');
+    } else {
+      miImage.setAttribute('src', 'images/guitarra.jpg');
+    }
+}
+
+let miBoton = document.querySelector('button');
+let misubtitulo = document.querySelector( 'h2');
+
+function estableceNombreUsuario() {
+    let miNombre = prompt('Por favor, ingresa tu nombre.');
+    localStorage.setItem('nombre', miNombre);
+    misubtitulo.textContent = 'Espero que disfrutes de mi música,' + miNombre;
+}
+
+if (!localStorage.getItem('nombre')) {
+    estableceNombreUsuario();
+}
+else {
+    let nombreAlmacenado = localStorage.getItem('nombre');
+    misubtitulo.textContent = 'Espero que disfrutes de mi música,' + nombreAlmacenado;
+}
+
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
+
+function estableceNombreUsuario() {
+    let miNombre = prompt('Introduzca su nombre.');
+    if(!miNombre) {
+      estableceNombreUsuario();
+    } else {
+      localStorage.setItem('nombre', miNombre);
+      misubtitulo.innerHTML = 'Espero que disfrutes de mi música, ' + miNombre;
+    }
+  }
